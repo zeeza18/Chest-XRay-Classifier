642a0e578c901e08621242636353be35a952354a
Mohammed Azeezulla <mdazeezulla2001@gmail.com>
2024-07-16 10:17:10 -0500

    bucket name change

--- Xray/constant/training_pipeline/__init__.py
+++ Xray/constant/training_pipeline/__init__.py
@@ -9,7 +9,7 @@ TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 # Data Ingestion Constants
 ARTIFACT_DIR: str = "artifacts"
 
-BUCKET_NAME: str = "lungxray24"
+BUCKET_NAME: str = "lungxray-zeeza"
 
 S3_DATA_FOLDER: str = "data"
 
